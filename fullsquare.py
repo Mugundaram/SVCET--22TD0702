ccbfa00e0aa3dbf7cf7dc8ea1b1598045765ac75
MUGUNDARAM <mugundaram22td0702@svcet.ac.in>
2025-01-31 15:07:41 +0530

    Add files via upload

---
+++ fullsquare.py
@@ -0,0 +1,11 @@
+side = 10
+i = 0
+print("Square Star Pattern") 
+
+while(i < side):
+    j = 0
+    while(j < side):      
+        j = j + 1
+        print('*', end = '  ')
+    i = i + 1
+    print('')
